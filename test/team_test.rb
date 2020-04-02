b43715828c364bca9d6a6e4092f75cd4af0d362a
Nicola Kim <nicola.copsey@gmail.com>
2020-04-02 14:52:46 -0700

    Iteration 2 complete

--- test/team_test.rb
+++ test/team_test.rb
@@ -16,5 +16,39 @@ class TeamTest < Minitest::Test
     assert_equal "France", team.country
   end
 
+  def test_team_is_not_eliminated
+    team = Team.new("France")
+    assert_equal false, team.eliminated?
+  end
+
+  def test_team_is_eliminated
+    team = Team.new("France")
+    team.eliminated = true
+    assert_equal true, team.eliminated?
+  end
+
+  def test_the_team_has_players
+    team = Team.new("France")
+    assert_equal [], team.players
+  end
+
+  def test_the_team_can_add_players
+    team = Team.new("France")
+    mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+    team.add_player(mbappe)
+    team.add_player(pogba)
+    assert_equal [mbappe, pogba], team.players
+  end
+
+  def test_the_team_has_players_by_position
+    team = Team.new("France")
+    mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+    team.add_player(mbappe)
+    team.add_player(pogba)
+    assert_equal [pogba], team.players_by_position("midfielder")
+    assert_equal [], team.players_by_position("defender")
+  end
 
 end
